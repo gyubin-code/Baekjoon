4777443d1ab5fc15693108886c2a54c590f9d9d3
gyubin-code <sdhg12@naver.com>
2021-07-21 23:35:57 +0900

    "21-07-21 주석추가"

--- 8979.py
+++ 8979.py
@@ -1,4 +1,6 @@
 import sys
+#k 나라보다 몇 나라가 위에있는지 ,, 알기위해 
+
 
 n, k = map(int, input().split())
 rank_table=[]
@@ -11,14 +13,14 @@ for _ in range(n):
 
 count = 1
 for i in range(len(rank_table)):
-    if rank_table[i][1] > standard[1]:
+    if rank_table[i][1] > standard[1]: #k나라보다 금메달 많은경어
         count += 1
 
-    elif rank_table[i][1] == standard[1]:
+    elif rank_table[i][1] == standard[1]: #k나라보다 은메달이 많은경어
         if rank_table[i][2] > standard[2]:
             count += 1
 
-        elif rank_table[i][2] == standard[2]:
+        elif rank_table[i][2] == standard[2]: #동메달이 많은경우
             if rank_table[i][3] > standard[3]:
                 count += 1
     
